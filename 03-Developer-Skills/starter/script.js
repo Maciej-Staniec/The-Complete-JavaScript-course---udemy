22a1956dc0ca819ff05ebb54b131f31fb38c5bcc
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-01-01 14:39:04 +0000

    commit

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -55,10 +55,103 @@ const helloWorld = "hello world";
 // How to merge two arrays? - use concat()
 // Do I need a new variable to hold merged arrays? - Yes, because concat() returns a new array, without changing the existing one.
 
+// const temps1 = [3, -2, -6, -1, "error", 9, 13, 17, "error", 15, 14, 9, 5];
+// const temps2 = [12, -10, -4, -1, "error", 10, "error", 22, 34, "error", 2, 4];
+
+// const calcTempAmplitude = function (temps1, temps2) {
+//   const concatArray = temps1.concat(temps2);
+//   let max = concatArray[0];
+//   let min = concatArray[0];
+//   for (let i = 0; i < concatArray.length; i++) {
+//     const currentTemp = concatArray[i];
+//     // console.log(i, typeof concatArray[i], concatArray[i]);
+//     if (typeof currentTemp === "number") {
+//       if (currentTemp > max) max = currentTemp;
+//       if (currentTemp < min) min = currentTemp;
+//     } else {
+//       console.log("string!");
+//       continue;
+//     }
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// console.log(calcTempAmplitude(temps1, temps2));
+
+// Coding Challenge
+
+// 1. Understanding the problem
+// Create a function that will accept an array to an 'arr' parameter.
+// How to print unknown number of values? - use for loop and loop over the length of the array
+// How to print the a string containing the forecast? - concatenate those values to one string
+// How to change the content of a forecast based on temperature value? use a string literal (template string) - `text`
+
+// 2. Breaking up into sub problems
+// Create an empty string to which a new string literal will be added on each iteration.
+// How to concatenate each new string to the existing one?
+// - use var1.concat(var2) method
+// - use var += var2
+
+// const printForecast = function (arr) {
+//   let forecast = ``;
+//   for (let i = 0; i < arr.length; i++) {
+//     forecast += `${arr[i]}°C in 1 days ... `;
+//   }
+//   return forecast;
+// };
+
+// console.log(printForecast([17, 21, 23]));
+// console.log(printForecast([12, 5, -5, 0, 4]));
+
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: "temp",
+//     unit: "celsius",
+//     value: prompt("Degrees Celsius:"),
+//   };
+//   console.log(measurement);
+//   console.log(measurement.value, typeof measurement.value);
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+
+// 1. Identify the bug
+// - We identified that the bug exists as the result of the function isn't as expected.
+// console.log(measureKelvin());
+
+// 2. Find the bug.
+// - Since the bug is in the converted 'kelvin' value, let's check all the other values that make up the calculation of this value.
+// - Check what is the value of the 'value' variable after prompt?
+// - Check their type?
+// - We found out that the 'value' is a string and '+' operator does type coercion on '273', and then concatenate both strings.
+
+// 3. Fix the bug.
+// - Convert the prompt 'value' to a number.
+
+// 4. Prevent the bug.
+// - convert it right after getting the user's input.
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: "temp",
+//     unit: "celsius",
+//     value: Number(prompt("Degrees Celsius:")),
+//   };
+//   console.table(measurement);
+//   console.log(measurement.value, typeof measurement.value);
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+
+// console.log(measureKelvin());
+
+// Using debugger.
+// Let's introduce bigger bug using the amplitude function we created earlier.
+
 const temps1 = [3, -2, -6, -1, "error", 9, 13, 17, "error", 15, 14, 9, 5];
 const temps2 = [12, -10, -4, -1, "error", 10, "error", 22, 34, "error", 2, 4];
 
-const calcTempAmplitude = function (temps1, temps2) {
+const calcTempAmplitudeBug = function (temps1, temps2) {
   const concatArray = temps1.concat(temps2);
   let max = concatArray[0];
   let min = concatArray[0];
@@ -77,29 +170,4 @@ const calcTempAmplitude = function (temps1, temps2) {
   return max - min;
 };
 
-console.log(calcTempAmplitude(temps1, temps2));
-
-// Coding Challenge
-
-// 1. Understanding the problem
-// Create a function that will accept an array to an 'arr' parameter.
-// How to print unknown number of values? - use for loop and loop over the length of the array
-// How to print the a string containing the forecast? - concatenate those values to one string
-// How to change the content of a forecast based on temperature value? use a string literal (template string) - `text`
-
-// 2. Breaking up into sub problems
-// Create an empty string to which a new string literal will be added on each iteration.
-// How to concatenate each new string to the existing one?
-// - use var1.concat(var2) method
-// - use var += var2
-
-const printForecast = function (arr) {
-  let forecast = ``;
-  for (let i = 0; i < arr.length; i++) {
-    forecast += `${arr[i]}°C in 1 days ... `;
-  }
-  return forecast;
-};
-
-console.log(printForecast([17, 21, 23]));
-console.log(printForecast([12, 5, -5, 0, 4]));
+console.log(calcTempAmplitudeBug(temps1, temps2));
