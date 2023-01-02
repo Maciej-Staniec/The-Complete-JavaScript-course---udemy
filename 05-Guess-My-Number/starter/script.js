50a50d65e43a8428596edca560c7e024aa5fd435
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-01-02 14:51:59 +0000

    commit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,39 +4,62 @@
 
 let msg = document.querySelector('.message');
 let number = document.querySelector('.number');
-let highscore = document.querySelector('.highscore');
+let highscoreSelector = document.querySelector('.highscore');
 const btnAgain = document.querySelector('.again');
 const btnCheck = document.querySelector('.check');
 // Variables
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-number.textContent = secretNumber;
+
 let score = 20;
+let highscore = 0;
 
 // Event listeners
 
 btnCheck.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
   if (!guess) {
     msg.textContent = '⛔No number!';
+
+    // When player wins
   } else if (guess === secretNumber) {
     msg.textContent = 'Correct number!';
+    number.textContent = secretNumber;
+    number.style.width = '30rem';
+    document.body.style.backgroundColor = '#60B347';
+    let currentScore = Number(document.querySelector('.score').textContent);
+    if (currentScore > highscore) {
+      highscore = currentScore;
+      highscoreSelector.textContent = highscore;
+    }
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       score--;
       document.querySelector('.score').textContent = score;
       msg.textContent = 'Too high!';
+      // When the score is 0 and player loose
     } else {
       document.querySelector('.score').textContent = 0;
       msg.textContent = 'You lost.';
     }
+    // When the guess is too low
   } else {
     if (score > 1) {
       score--;
       document.querySelector('.score').textContent = score;
       msg.textContent = 'Too low';
+
+      // When the score is 0 and player loose
     } else {
       document.querySelector('.score').textContent = 0;
       msg.textContent = 'You lost.';
     }
   }
+});
+
+btnAgain.addEventListener('click', function () {
+  score = 20;
 });
