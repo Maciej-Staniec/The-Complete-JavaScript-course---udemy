9a0877b8c37a43a691ed04786e9582a4a6057ae0
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-01-03 21:20:19 +0000

    commit

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,16 +6,36 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnShowModal = document.querySelectorAll(".show-modal");
 
-// Removing .hidden class from the modal and overlay classes
-for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
-}
-
-// Adding .hidden class to the modal and overlay
-btnCloseModal.addEventListener("click", function () {
+// Functions
+const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+};
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// Opening modal when one of the modal buttons clicked
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", openModal);
+}
+
+// Exiting modal on X button click
+btnCloseModal.addEventListener("click", closeModal);
+
+// Exiting modal on overlay click
+overlay.addEventListener("click", closeModal);
+
+// "Esc" key to exit the modal
+// By adding event listener on the 'document' object, we listen to the key everywhere, no matter where they happen on the page.
+// The event handler (the function we use inside the event listener) can get the event's information as a parameter, which happens once the event is triggered. Once the event occurs, the event object is created and passed to the event handler so we can access its information. The event object contains all information about the event that triggered the listener.
+document.addEventListener("keydown", function (e) {
+  if (
+    e.key === "Escape" &&
+    !modal.classList.contains("hidden") &&
+    !overlay.classList.contains("hidden")
+  )
+    closeModal();
 });
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,168 @@
-'use strict';
+"use strict";
+// --------------------------Selectors----------------------------
+// Values
+const leftScoreSelector = document.querySelector("#score--0");
+const rightScoreSelector = document.querySelector("#score--1");
+const leftCurrentScoreSelector = document.querySelector("#current--0");
+const rightCurrentScoreSelector = document.querySelector("#current--1");
+const leftPlayer = document.querySelector("#name--0");
+const rightPlayer = document.querySelector("#name--1");
+// Images
+const diceSelector = document.querySelector(".dice");
+// Buttons
+const btnRollDiceSelector = document.querySelector(".btn--roll");
+const btnHoldSelector = document.querySelector(".btn--hold");
+const btnNewGameSelector = document.querySelector(".btn--new");
+// Sections
+const leftSectionSelector = document.querySelector(".player--0");
+const rightSectionSelector = document.querySelector(".player--1");
+// Variables
+let leftScore = 0;
+let leftCurrentScore = 0;
+
+let rightScore = 0;
+let rightCurrentScore = 0;
+
+let player1 = true;
+
+// --------------------------Functions---------------------------
+const roll = function () {
+  let diceValue = Math.trunc(Math.random() * 6) + 1;
+  diceSelector.src = `dice-${diceValue}.png`;
+  diceSelector.classList.remove("hidden");
+  return diceValue;
+};
+
+const disableButtons = function () {
+  btnRollDiceSelector.style.cursor = "not-allowed";
+  btnRollDiceSelector.disabled = true;
+  btnHoldSelector.style.cursor = "not-allowed";
+  btnHoldSelector.disabled = true;
+};
+
+const activateButtons = function () {
+  btnRollDiceSelector.style.cursor = "pointer";
+  btnRollDiceSelector.disabled = false;
+  btnHoldSelector.style.cursor = "pointer";
+  btnHoldSelector.disabled = false;
+};
+
+const activatePlayer = function () {
+  if (player1 === true) {
+    player1 = false;
+    leftSectionSelector.classList.remove("player--active");
+    rightSectionSelector.classList.add("player--active");
+  } else {
+    player1 = true;
+    rightSectionSelector.classList.remove("player--active");
+    leftSectionSelector.classList.add("player--active");
+  }
+};
+
+const rollDice = function () {
+  let diceValue = roll();
+  console.log(diceValue);
+  diceSelector.src = `dice-${diceValue}.png`;
+  diceSelector.classList.remove("hidden");
+  console.log(player1);
+  if (player1) {
+    if (diceValue != 1) {
+      leftCurrentScore += diceValue;
+      leftCurrentScoreSelector.textContent = leftCurrentScore;
+    } else {
+      leftCurrentScore = 0;
+      leftCurrentScoreSelector.textContent = 0;
+      activatePlayer();
+    }
+  } else {
+    if (diceValue != 1) {
+      rightCurrentScore += diceValue;
+      rightCurrentScoreSelector.textContent = rightCurrentScore;
+    } else {
+      rightCurrentScore = 0;
+      rightCurrentScoreSelector.textContent = 0;
+      activatePlayer();
+    }
+  }
+};
+
+const hold = function () {
+  if (!diceSelector.classList.contains("hidden")) {
+    if (player1) {
+      if (leftScore >= 100) {
+        leftPlayer.textContent = "WINNER!";
+        rightPlayer.textContent = "LOSER!";
+        leftSectionSelector.classList.add("player--winner");
+        leftScore += leftCurrentScore;
+        leftScoreSelector.textContent = leftScore;
+        console.log(leftSectionSelector);
+        leftCurrentScore = 0;
+        leftCurrentScoreSelector.textContent = leftCurrentScore;
+        disableButtons();
+      } else {
+        leftScore += leftCurrentScore;
+        leftScoreSelector.textContent = leftScore;
+        leftCurrentScore = 0;
+        leftCurrentScoreSelector.textContent = leftCurrentScore;
+        activatePlayer();
+        rollDice();
+      }
+    } else {
+      if (rightScore >= 100) {
+        rightPlayer.textContent = "WINNER!";
+        leftPlayer.textContent = "LOSER!";
+        rightSectionSelector.classList.add("player--winner");
+        rightScore += rightCurrentScore;
+        rightScoreSelector.textContent = rightScore;
+        rightCurrentScore = 0;
+        rightCurrentScoreSelector.textContent = rightCurrentScore;
+        disableButtons();
+      } else {
+        rightScore += rightCurrentScore;
+        rightScoreSelector.textContent = rightScore;
+        rightCurrentScore = 0;
+        rightCurrentScoreSelector.textContent = rightCurrentScore;
+        activatePlayer();
+        rollDice();
+      }
+    }
+  }
+};
+
+const newGame = function () {
+  // Reset players score
+
+  leftScore = 0;
+  leftScoreSelector.textContent = 0;
+  leftCurrentScoreSelector.textContent = 0;
+
+  rightScore = 0;
+  rightScoreSelector.textContent = 0;
+  rightCurrentScoreSelector.textContent = 0;
+
+  // Select player 1 and Change the overlay to the left side if player2 was rolling a dice or won the game
+
+  // Hide the dice
+  diceSelector.classList.add("hidden");
+  // Change back to player 1
+  if (player1) player1 = false;
+
+  activatePlayer();
+  leftPlayer.textContent = "Player 1";
+  rightPlayer.textContent = "Player 2";
+
+  leftSectionSelector.classList.contains("player--winner")
+    ? leftSectionSelector.classList.remove("player--winner")
+    : rightSectionSelector.classList.remove("player--winner");
+
+  activateButtons();
+};
+
+// -----------------------Listeners------------------------------
+
+// Listener for Roll dice button
+btnRollDiceSelector.addEventListener("click", rollDice);
+// Listener for Hold button
+btnHoldSelector.addEventListener("click", hold);
+// Listener for New Game button
+btnNewGameSelector.addEventListener("click", newGame);
