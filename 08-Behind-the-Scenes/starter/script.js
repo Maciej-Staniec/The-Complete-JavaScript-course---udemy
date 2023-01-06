1e136fe673a65cd98129be4242e63768e6fb9cb9
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-01-06 10:01:27 +0000

    commit

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,59 +1,47 @@
 'use strict';
 
-const myName = 'Jonas'; // Global variable
+// function calcAge(birthYear) {
+//   // const firstName = 'Steven';
+//   const age = 2037 - birthYear;
+//   function printAge() {
+//     let output = `output = You are ${age}, born in ${birthYear}`;
+//     console.log(output);
+//     var firstName = 'Maciek';
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       const output = `ouput = OUTPUT HAS CHANGED`;
+//       const str = `You are a millenial, ${firstName}`;
+//       console.log(str);
 
-// console.log(millenial); //Reference error - not defined
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       add(2, 3);
+//     }
+//     console.log(output);
+//     console.log(millenial);
+//     // add(2, 3);
+//   }
 
-function first() {
-  const age = 30;
+//   function printName() {
+//     console.log(firstName); // Notice, that 'Maciek' value declared as a firstName variable with a var keyword isn't logged. It's because 'var' keyword makes variables available only in the closest function scope.
+//     // If we comment out firstName in the global scope, we get referrence error - variable undefined
+//   }
+//   printAge();
+//   printName();
+//   console.log(firstName);
+//   return age;
+// }
 
-  if (age >= 30) {
-    const decade = 3; // Scope variable
-    var millenial = true;
-    // console.log(lastName); //Reference error - not defined
-  }
-  console.log(millenial);
-  // console.log(decade); //Reference error - not defined
-  function second() {
-    const job = 'teacher';
-    var experience = 'true';
-    // console.log(decade); //Reference error - not defined
-    console.log(millenial);
-    console.log(`${myName} is a ${age} years old ${job}`);
+// const firstName = 'Jonas';
 
-    if (millenial) {
-      var greeting = 'Hello!';
-      const goodbye = 'Goodbye!';
-      console.log(experience);
+// calcAge(1991);
 
-      function greet() {
-        var fullGreeting = 'Hello World!';
-        console.log(fullGreeting);
-        console.log(experience);
-        console.log(goodbye);
-        if (true) {
-          console.log(fullGreeting);
-        }
-      }
+console.log(me);
+// console.log(job);
+// console.log(birthYear);
 
-      greet();
-      // console.log(fullGreeting);
-    }
-    console.log(greeting);
-  }
+var me = 'Maciek';
+let job = 'student';
+const birthYear = 1991;
 
-  // console.log(experience); //Reference error - not defined
-
-  function third() {
-    var lastName = 'Staniec';
-    // console.log(experience); //Reference error - not defined
-    console.log(millenial);
-  }
-
-  // console.log(greeting); //Reference error - not defined
-
-  second();
-  third();
-}
-
-first();
